50ce6170dbf0214033c45a1bd450b88df56d957d
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-12-26 20:35:04 +0100

    Populate encoding field appropriately

--- search/models.py
+++ search/models.py
@@ -1,7 +1,12 @@
 import uuid
 
+import face_recognition
+
+from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import UploadedFile
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 from cloudinary.models import CloudinaryField
 
@@ -38,7 +43,29 @@ class ScumShot(AbstractShrewdModel):
     encoding = NumpyArrayField()
 
     def __str__(self):
-        return self.shot.url
+        try:
+            return self.shot.url
+        except AttributeError:
+            return super().__str__()
+
+    def clean(self, *args, **kwargs):
+        if isinstance(self.shot, UploadedFile):
+            image = face_recognition.load_image_file(self.shot)
+            face_locations = face_recognition.face_locations(image)
+
+            if len(face_locations) < 1:
+                raise ValidationError({"shot": _("No human face found in shot")})
+            elif len(face_locations) > 1:
+                raise ValidationError(
+                    {"shot": _("More than one human face found in shot")}
+                )
+
+            face_encodings = face_recognition.face_encodings(
+                image, known_face_locations=face_locations, num_jitters=4
+            )
+            self.encoding = face_encodings[0]
+
+        super().clean(*args, **kwargs)
 
 
 class Scum(AbstractShrewdModel):

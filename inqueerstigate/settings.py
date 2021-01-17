297daccf4a8ccd13ffffc27453e4c4302633b121
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-01-17 11:24:31 +0100

    Iron out static file serving in dev mode

--- inqueerstigate/settings.py
+++ inqueerstigate/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "static_root"
+STATIC_ROOT = os.getenv("STATIC_ROOT")
 STATICFILES_DIRS = ["static"]
 
 # Media files

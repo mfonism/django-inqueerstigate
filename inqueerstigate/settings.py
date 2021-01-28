e00fa258045a18a2be2d04b97b8af34a38df52be
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-01-28 16:12:48 +0100

    Clean up env vars and settings

--- inqueerstigate/settings.py
+++ inqueerstigate/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = ["static"]
 
 # Media files
